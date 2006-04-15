f546938244373ea7800c1fa61f8a9009fa1ea3cf
modemhero <unknown>
2006-04-15 12:10:57 +0000

    [project @ 2006-04-15 12:10:57 by modemhero]
    Typo fix in new army capacity code.

--- Vanilla/ntserv/daemonII.c
+++ Vanilla/ntserv/daemonII.c
@@ -2948,7 +2948,7 @@ static void beam(void)
                 if (j->p_armies >= (int)((float)((int)(j->p_kills*100)/100.0) * 3.0))
                     continue;
             } else if (j->p_ship.s_type != STARBASE)
-                if (j->p_armies >= (int)((float)(int)(j->p_kills*100)/100.0) * 2.0))
+                if (j->p_armies >= (int)((float)((int)(j->p_kills*100)/100.0) * 2.0))
                     continue;
             if (j->p_flags & PFORBIT) {
                 if (j->p_team != l->pl_owner)
