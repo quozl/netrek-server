1071049ab4c6824f00aae01f9112bbd4b801f296
quozl <quozl@us.netrek.org>
2006-09-19 11:33:17 +0000

    ut06 packaging fixes

--- Vanilla/autogen.sh
+++ Vanilla/autogen.sh
@@ -5,4 +5,5 @@ autoconf
 cd res-rsa && autoconf && cd ..
 chmod +x tools/mktrekon
 chmod +x tools/admin/*
+chmod +x debian/rules debian/postinst debian/postrm debian/init
 echo "autogen.sh completed ok"
