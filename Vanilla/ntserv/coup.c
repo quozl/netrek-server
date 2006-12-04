d426ec3b598f4a9f3c6a4e3b01f75f30fc05bded
williamb <williamb@its.caltech.edu>
2006-12-04 05:42:19 +0000

    Toggle special weapons
         * coup.c: Adds additional functionality to the rarely used coup key,
           making it the toggle key to cycle between special weapon types on
           the player's ship.  First checks if in a position to actually do
           a coup.
    M ./Vanilla/ntserv/coup.c +39

--- Vanilla/ntserv/coup.c
+++ Vanilla/ntserv/coup.c
@@ -1,6 +1,7 @@
 /*
  * coup.c
  */
+#include <stdio.h>
 #include "copyright.h"
 #include "defs.h"
 #include "struct.h"
@@ -13,6 +14,44 @@ void coup(void)
 {
     register int i;
     register struct planet *l;
+
+#ifdef STURGEON
+    /* Coup key (rarely used) doubles as toggle key to switch between various
+       special weapons in inventory */
+    l = &planets[me->p_planet];
+    /* Make sure player not actually in a position to coup */
+    if (sturgeon && (!(me->p_flags & PFORBIT) ||
+        !(l->pl_flags & PLHOME) ||
+        (l->pl_owner == me->p_team) ||
+        !(l->pl_flags & me->p_team)))
+    {
+        char buf[80];
+        int t = me->p_special;
+        if (t < 0) {
+            new_warning(UNDEF,"This ship is not equipped with special weapons.");
+            return;
+        }
+        for (t = (t+1) % NUMSPECIAL; me->p_weapons[t].sw_number == 0 &&
+             t != me->p_special; t = (t+1) % NUMSPECIAL)
+            ;
+
+        if ((t == me->p_special) && (me->p_weapons[t].sw_number == 0)) {
+            new_warning(UNDEF, "We have exhausted all of our special weapons, Captain.");
+            me->p_special = -1;
+        }
+        else {
+            if (me->p_weapons[t].sw_number < 0)  /* infinite */
+                sprintf(buf, "Special Weapon: %s", me->p_weapons[t].sw_name);
+            else
+                sprintf(buf, "Special Weapon: %s (%d left)",
+                    me->p_weapons[t].sw_name, me->p_weapons[t].sw_number);
+
+            new_warning(UNDEF,buf);
+            me->p_special = t;
+        }
+        return;
+    }
+#endif
 
     if (me->p_kills < 1.0) {
         new_warning(3,"You must have one kill to throw a coup");
