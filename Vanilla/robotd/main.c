6456936ee7b84d7a98c909efa46b89a625f44b7c
quozl <quozl@us.netrek.org>
2006-12-14 05:05:39 +0000

    robotd, re-order option parsing
    Re-ordering of option parsing to make it clearer where room remains in the
    alphabet for addition of new options, standardised a comment on each option
    for ease of maintaining usage, laid out usage by classification of option.

--- Vanilla/robotd/main.c
+++ Vanilla/robotd/main.c
@@ -81,109 +81,12 @@ main(argc, argv)
       ptr = *argv++;
       while (*ptr) {
 	 switch (*ptr) {
-	 case 'v':
-	    usage++;
-	    break;
-	 case 'u':
-	    ptr++;
-	    mprintf("ptr: %s\n", ptr);
-	    sscanf(ptr, "%f", &updates);
-	    mprintf("updates at %g (%d per second)\n", updates,
-	       (int)(1000000./(updates * 100000.)));
-	    ptr += strlen(ptr)-1;
-	    break;
-	    
-	 case 'U':
-	    if(*(ptr+1)){
-	       ptr++;
-	       switch(*ptr){
-		  case 'f': 
-		     udpClientRecv = 2; 
-		     break;
-		  case 's': 
-		     udpClientRecv = 1;
-		     break;
-	       }
-	    }
-	    tryudp++;
-	    mprintf("tryudp on, udp recv mode %d\n", udpClientRecv);
+
+	 case 'b': /* argv opt, -b : blind, don't read from stdin */
+	    read_stdin = 0;
 	    break;
 
-	 case 's':
-	    if (*argv) {
-	       xtrekPort = atoi(*argv);
-	       passive = 1;
-	       argv++;
-	       argc--;
-	    }
-	    break;
-
-	 case 'S':
-	    shmem = 1;
-	    break;
-
-	 case 'p':
-	    if (*argv) {
-	       xtrekPort = atoi(*argv);
-	       argv++;
-	       argc--;
-	    }
-	    break;
-	 case 'i':
-	    inl = 1;
-	    updates = 2.0;
-	    break;
-
-	 case 'h':
-	    serverName = *argv;
-	    argc--;
-	    argv++;
-	    break;
-	 case 'r':
-	    defaultsFile = *argv;
-	    argv++;
-	    argc--;
-	    break;
-	 case 'n':		/* NAME */
-	    strncpy(pseudo, *argv, sizeof(pseudo));
-	    pseudo[sizeof(pseudo) - 1] = '\0';
-	    argv++;
-	    argc--;
-	    break;
-	 
-	 case 'x':
-	    strcpy(password, *argv);
-	    argv++;
-	    argc--;
-	    break;
-	 case 'X':
-	    strncpy(login, *argv, sizeof(login));
-	    argv++;
-	    argc--;
-	    break;
-	 
-	 case 'T':		/* TEAM */
-	    ptr++;
-	    switch (*ptr) {
-	    case 'f':
-	       team = FED_N;
-	       break;
-	    case 'r':
-	       team = ROM_N;
-	       break;
-	    case 'k':
-	       team = KLI_N;
-	       break;
-	    case 'o':
-	       team = ORI_N;
-	       break;
-	    default:
-	       team = -1;
-	       break;
-	    }
-	    break;
-
-	 case 'c':		/* SHIP */
+	 case 'c': /* argv opt, -c[sdcbago] : ship type */
 	    ptr++;
 	    switch (*ptr) {
 	    case 's':
@@ -212,38 +115,141 @@ main(argc, argv)
 	       break;
 	    }
 	    break;
-	 
-	 case 'b':	/* blind */
-	    read_stdin = 0;
-	    break;
-	 
-	 case 'o':
-	    override = 1;
-	    break;
-	 
-	 case 'O':
-	    nopwd = 1;
-	    break;
-	 
-	 case 'R':
-#ifndef PUBLIC
-	    doreserved = 1;
-#endif
-	    break;
-	 
-	 case 'C':
+
+	 case 'C': /* argv opt, -C file : initial commands file */
 	    commfile = *argv;
 	    argv++;
 	    argc--;
 	    break;
-	 
-	 case 'l':
+
+	 case 'g': /* argv opt, -g : tell server we are a robot */
+	    oggv_packet = 1;
+	    break;
+
+	 case 'h': /* argv opt, -h server : server host name or ip */
+	    serverName = *argv;
+	    argc--;
+	    argv++;
+	    break;
+
+	 case 'i': /* argv opt, -i : set inl flag */
+	    inl = 1;
+	    updates = 2.0;
+	    break;
+
+	 case 'I': /* argv opt, -I : ignore lack of t-mode */
+	    ignoreTMode = 1;
+	    break;
+
+	 case 'l': /* argv opt, -l logfile : log into log file */
 	    setlog(*argv);
 	    argv++;
 	    argc--;
 	    break;
 
-	 case 'w':
+	 case 'n': /* argv opt, -n name : set robot player name */
+	    strncpy(pseudo, *argv, sizeof(pseudo));
+	    pseudo[sizeof(pseudo) - 1] = '\0';
+	    argv++;
+	    argc--;
+	    break;
+
+	 case 'o': /* argv opt, -o : override decision making */
+	    override = 1;
+	    break;
+
+	 case 'O': /* argv opt, -O : disable robot in-game control password */
+	    nopwd = 1;
+	    break;
+
+	 case 'p': /* argv opt, -p port : connect to server port */
+	    if (*argv) {
+	       xtrekPort = atoi(*argv);
+	       argv++;
+	       argc--;
+	    }
+	    break;
+
+	 case 'q': /* argv opt, -q : unknown */
+	    noargs ++;
+	    break;
+
+	 case 'r': /* argv opt, -r file : read from defaults file */
+	    defaultsFile = *argv;
+	    argv++;
+	    argc--;
+	    break;
+
+	 case 'R': /* argv opt, -R : doreserved = 1 */
+#ifndef PUBLIC
+	    doreserved = 1;
+#endif
+	    break;
+
+	 case 's': /* argv opt, -s port : accept ntserv connection on port */
+	    if (*argv) {
+	       xtrekPort = atoi(*argv);
+	       passive = 1;
+	       argv++;
+	       argc--;
+	    }
+	    break;
+
+	 case 'S': /* argv opt, -S : shmem = 1*/
+	    shmem = 1;
+	    break;
+
+	 case 'T': /* argv opt, -T[frko] : set team to join */
+	    ptr++;
+	    switch (*ptr) {
+	    case 'f':
+	       team = FED_N;
+	       break;
+	    case 'r':
+	       team = ROM_N;
+	       break;
+	    case 'k':
+	       team = KLI_N;
+	       break;
+	    case 'o':
+	       team = ORI_N;
+	       break;
+	    default:
+	       team = -1;
+	       break;
+	    }
+	    break;
+
+	 case 'u': /* argv opt, -u updates : set updates per second */
+	    ptr++;
+	    mprintf("ptr: %s\n", ptr);
+	    sscanf(ptr, "%f", &updates);
+	    mprintf("updates at %g (%d per second)\n", updates,
+	       (int)(1000000./(updates * 100000.)));
+	    ptr += strlen(ptr)-1;
+	    break;
+
+	 case 'U': /* argv opt, -U[fs] : set UDP mode */
+	    if(*(ptr+1)){
+	       ptr++;
+	       switch(*ptr){
+		  case 'f': 
+		     udpClientRecv = 2; 
+		     break;
+		  case 's': 
+		     udpClientRecv = 1;
+		     break;
+	       }
+	    }
+	    tryudp++;
+	    mprintf("tryudp on, udp recv mode %d\n", udpClientRecv);
+	    break;
+
+	 case 'v': /* argv opt, -v : emit usage summary */
+	    usage++;
+	    break;
+
+	 case 'w': /* argv opt, -w : trigger rwatch */
 	    rwatch = 1;
 	    if(gethostname(rw_host, 80) < 0){
 	       perror("gethostname");
@@ -252,23 +258,24 @@ main(argc, argv)
 	    break;
 
 #ifdef nodef
-	 case 'W':
+	 case 'W': /* argv opt, -W host : set rwatch hostname */
 	    rwatch = 2;
 	    strcpy(rw_host, *argv);
 	    argv++;
 	    argc--;
 	    break;
 #endif
-	 case 'q':
-	    noargs ++;
-	    break;
-	 
-	 case 'g':
-	    oggv_packet = 1;
+
+	 case 'x': /* argv opt, -x password : use login password */
+	    strcpy(password, *argv);
+	    argv++;
+	    argc--;
 	    break;
 
-	 case 'I':
-	    ignoreTMode = 1;
+	 case 'X': /* argv opt, -X login : user login name */
+	    strncpy(login, *argv, sizeof(login));
+	    argv++;
+	    argc--;
 	    break;
 
 	 default:
@@ -479,33 +486,44 @@ main(argc, argv)
 printUsage(prog)
    char           *prog;
 {
-static char	*list[] = {
-   "   -v               : show usage",
-   "   -u[updates]	: set server update timer",
-   "   -p <port>        : port number",
-   "   -h <server>      : server name",
-   "   -r <df>          : defaults file",
-   "   -n <name>        : player name",
-   "   -x <pwd>         : player password",
-   "   -X <login>       : login name",
-   "   -T[frko]         : team letter",
-   "   -c[sdcbago]      : ship type",
-   "   -C <file>        : initial commands file",
-   "   -b               : don't read from stdin",
-   "   -o               : override decision making",
-   "   -w <host>        : connect to rwatch on host",
-   "   -l <logfile>     : set log file",
-   "   -O               : no password to use robot",
+   static char *list[] = {
+   "network connection options",
+   "  -h server   : server host name or ip (critical)",
+   "  -p port     : connect to server port",
+   "  -s port     : accept ntserv connection on port",
+   "  -U[fs]      : set UDP mode",
+   "authentication and identification options",
+   "  -n name     : set robot player name",
+   "  -X login    : user login name",
+   "  -x password : use login password",
+   "  -g          : tell server we are a robot",
+   "team and ship selection options",
+   "  -T[frko]    : set team to join",
+   "  -c[sdcbago] : ship type",
+   "in-game behaviour options",
+   "  -b          : blind, don't read from stdin",
+   "  -C file     : initial commands file",
+   "  -I          : ignore lack of t-mode",
+   "  -O          : disable robot in-game control password",
+   "  -o          : override decision making",
+   "  -u updates  : set updates per second",
+   "other options",
+   "  -i          : set inl flag",
+   "  -l logfile  : log into log file",
+   "  -q          : unknown",
+   "  -r file     : read from defaults file",
+   "  -R          : doreserved = 1",
+   "  -S          : shmem = 1",
+   "  -v          : emit usage summary",
+   "  -w          : trigger rwatch",
+   "  -W host     : set rwatch hostname",
    NULL,
 };
 
-   char		**s = list;
+   char **s;
 
    fprintf(stderr, "usage: %s [options]\n", prog);
-   fprintf(stderr, "where options include:\n");
-   for(; *s; s++){
-      fprintf(stderr, "%s\n", *s);
-   }
+   for(s = list; *s; s++) fprintf(stderr, "%s\n", *s);
    exit(0);
 }
 
