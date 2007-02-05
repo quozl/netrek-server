4a32850fc9028a5fa9458e06515286948b7a5b48
quozl <quozl@us.netrek.org>
2007-02-05 22:16:55 +0000

    chmod tests/build during autogen

--- Vanilla/autogen.sh
+++ Vanilla/autogen.sh
@@ -6,4 +6,5 @@ cd res-rsa && autoconf && cd ..
 chmod +x tools/mktrekon
 chmod +x tools/admin/*
 chmod +x debian/rules debian/postinst debian/postrm debian/init
+chmod +x tests/build
 echo "autogen.sh completed ok"
