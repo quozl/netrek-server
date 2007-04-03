4005c66939dc8bc9cc229f4c552566eb66dd3a2c
quozl <quozl@us.netrek.org>
2007-04-03 09:09:29 +0000

    prerelease 2.13.0

--- Vanilla/include/version.h
+++ Vanilla/include/version.h
@@ -1,9 +1,9 @@
 /* store the version info here */
 
 #ifdef SVERS
-static char serv_version[]="Vanilla server version 2.12";
+static char serv_version[]="Vanilla server version 2.13";
 #endif
 
 #ifdef MVERS
-static char mvers[]="2.12";
+static char mvers[]="2.13";
 #endif
