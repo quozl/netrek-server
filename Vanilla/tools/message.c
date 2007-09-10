983c884ec6fda8aba25b51b7b10988d63a62dea3
quozl <quozl@us.netrek.org>
2007-09-10 06:51:18 +0000

    inl, add test script
    	* tools/message.c: add a forge-to-self mode for sending commands to the
    	  INL robot on behalf of a player.
    
    	* tests/inl: add a test sequence outline for running an INL robot
    	  moderated game.

--- Vanilla/tools/message.c
+++ Vanilla/tools/message.c
@@ -3,21 +3,32 @@
 #include "defs.h"
 #include "struct.h"
 #include "data.h"
-
-/* file scope prototypes */
-static void pmessage(char *, int, int);
+#include "util.h"
 
 /* external prototypes */
 extern int openmem(int);	/* from openmem.c */
 
+void pmessage(char *str, int recip, int group)
+{
+    struct message *cur;
+    if (++(mctl->mc_current) >= MAXMESSAGE)
+	mctl->mc_current = 0;
+    cur = &messages[mctl->mc_current];
+    cur->m_no = mctl->mc_current;
+    cur->m_flags = group;
+    cur->m_time = 0;
+    cur->m_recpt = recip;
+    cur->m_from = 255; /* change 12/11/90 TC */
+    (void) sprintf(cur->m_data, "%s", str);
+    cur->m_flags |= MVALID;
+}
 
-int main()
+int prior()
 {
     char message[MSG_LEN + 20];
     char buf[MSG_LEN + 20];
     char *to;
 
-    openmem(0);
     printf("Enter Message:\n");
     fgets(message, MSG_LEN + 20, stdin);
     if (strlen(message) > 0) message[strlen(message)-1] = '\0';
@@ -79,17 +90,30 @@ int main()
     return 1;
 }
 
-void pmessage(char *str, int recip, int group)
+int main(int argc, char **argv)
 {
+    int i = 1;
+
+    openmem(0);
+    if (argc == 1) return prior();
+
+    if (strcmp(argv[i], "forge-to-self")) return 1;
+    if (++i == argc) return 1;
+
+    struct player *me = player_by_number(argv[i]);
+    if (me == NULL) return 1;
+    if (++i == argc) return 1;
+
     struct message *cur;
-    if (++(mctl->mc_current) >= MAXMESSAGE)
-	mctl->mc_current = 0;
+    if (++(mctl->mc_current) >= MAXMESSAGE) mctl->mc_current = 0;
     cur = &messages[mctl->mc_current];
     cur->m_no = mctl->mc_current;
-    cur->m_flags = group;
+    cur->m_flags = MINDIV;
     cur->m_time = 0;
-    cur->m_recpt = recip;
-    cur->m_from = 255; /* change 12/11/90 TC */
-    (void) sprintf(cur->m_data, "%s", str);
+    cur->m_recpt = me->p_no;
+    cur->m_from = me->p_no;
+    (void) sprintf(cur->m_data, " %s->%s   %s", me->p_mapchars, me->p_mapchars, argv[i]);
     cur->m_flags |= MVALID;
+
+    return 0;
 }
