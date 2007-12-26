cdffdfd82c08a14e17a204d39446532da04a3e93
Karthik Arumugham <karthik@karthik.com>
2007-12-26 21:34:03 +0000

    Fix to allow admin observers to rejoin any team after a team-change xtkill
        getentry.c: Sometimes admin observers can only rejoin one team. This
            appears to happen after being xtkilled to a different team.

--- Vanilla/ntserv/getentry.c
+++ Vanilla/ntserv/getentry.c
@@ -330,8 +330,11 @@ static int tournamentMask(int team, int queue)
         return 0;
     
     /* Handle special cases that do other things */
-    /* Chaos, topgun mode, or admin observer */
-    if (chaos || topgun || (me->p_authorised && (me->p_flags & PFOBSERV)))
+    /* admin observer */
+    if (me->p_authorised && (me->p_flags & PFOBSERV))
+        return ALLTEAM;
+    /* Chaos or topgun mode */
+    if (chaos || topgun)
         return mask;
     /* Queue with no restrictions */
     if (!(queues[queue].q_flags & QU_RESTRICT))
