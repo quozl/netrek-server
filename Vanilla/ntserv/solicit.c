005921b2d02c419d780e77dba61fd569be1c5bbc
quozl <quozl@us.netrek.org>
2009-05-24 23:39:27 +0000

    solicit, trailing whitespace

--- Vanilla/ntserv/solicit.c
+++ Vanilla/ntserv/solicit.c
@@ -55,7 +55,7 @@ static int udp_attach(struct metaserver *m)
     perror("solicit: udp_attach: socket");
     return 0;
   }
-  
+
   /* bind the local socket */
   /* bind to interface attached to this.host.name */
 
@@ -90,15 +90,15 @@ static int udp_attach(struct metaserver *m)
     perror("solicit: udp_attach: unable to bind to desired interface (this.host.name field)");
     return 0;
   }
-  
+
   /* build the destination address */
   m->address.sin_family = AF_INET;
   m->address.sin_port = htons(m->port);
-  
+
   /* attempt numeric translation first */
   if ((m->address.sin_addr.s_addr = inet_addr(m->host)) == -1) {
     struct hostent *hp;
-    
+
     /* then translation by name */
     if ((hp = gethostbyname(m->host)) == NULL) {
       /* if it didn't work, return failure and warning */
@@ -108,7 +108,7 @@ static int udp_attach(struct metaserver *m)
       memcpy( &(m->address.sin_addr.s_addr), hp->h_addr, 4);
     }
   }
-  
+
   return 1;
 }
 
@@ -122,7 +122,7 @@ static int udp_tx(struct metaserver *m, char *buffer, int length)
     perror("solicit: udp_tx: sendto");
     return 0;
   }
-  
+
   return 1;
 }
 
@@ -138,13 +138,13 @@ void solicit(int force)
   int gamefull = 0;               /* is the game full? */
   int isrsa = 0;                  /* is this server RSA? */
 
-  /* perform first time initialisation */ 
+  /* perform first time initialisation */
   if (initialised == 0) {
     FILE *file;
-    
+
     /* clear metaserver socket list */
     for (i=0; i<MAXMETASERVERS; i++) metaservers[i].sock = -1;
-    
+
     /* open the metaserver list file */
     file = fopen(SYSCONFDIR"/metaservers", "r");
     if (file == NULL) file = fopen(SYSCONFDIR"/.metaservers", "r");
@@ -152,14 +152,14 @@ void solicit(int force)
       initialised++;
       return;
     }
-    
+
     /* read the metaserver list file */
     for (i=0; i<MAXMETASERVERS; i++) {
       struct metaserver *m = &metaservers[i];
       char buffer[256];         /* where to hold the metaservers line */
       char *line;		/* return from fgets() */
       char *token;		/* current line token */
-      
+
       /* read a line */
       line = fgets(buffer, 256, file);
 
@@ -191,7 +191,7 @@ void solicit(int force)
       token = strtok(NULL, " ");	/* our host name */
       if (token == NULL) continue;
       strncpy(m->ours, token, 32);
-      
+
       token = strtok(NULL, " ");	/* server type */
       if (token == NULL) continue;
       strncpy(m->type, token, 2);
@@ -199,7 +199,7 @@ void solicit(int force)
       token = strtok(NULL, " ");	/* player port */
       if (token == NULL) continue;
       m->pport = atoi(token);
-      
+
       token = strtok(NULL, " ");	/* observer port */
       if (token == NULL) continue;
       m->oport = atoi(token);
@@ -213,12 +213,12 @@ void solicit(int force)
 	m->minimum = META_MINIMUM_DELAY;
       if (m->maximum > META_MAXIMUM_DELAY)
 	m->maximum = META_MAXIMUM_DELAY;
-      
+
       /* attach to the metaserver (DNS lookup is only delay) */
       udp_attach(m);
       /* place metaserver addresses in /etc/hosts to speed this */
       /* use numeric metaserver address to speed this */
-      
+
       /* initialise the other parts of the structure */
       m->sent = 0;
       strcpy(m->prior, "");
@@ -226,7 +226,7 @@ void solicit(int force)
     initialised++;
     fclose(file);
   }
-  
+
   /* Don't solicit in INL mode unless both sides have captains;
      this fixes the problem of the server sticking around post-game
      due to player slots remaining in the game */
@@ -250,7 +250,7 @@ void solicit(int force)
 
     /* skip empty metaserver entries */
     if (m->sock == -1) continue;
-    
+
     ERROR(9,("solicit[%d](ours:'%s' type='%s' pport=%d oport=%d meta='%s' mport=%d)\n", i, m->ours, m->type, m->pport, m->oport, m->host, m->port));
 
     /* only process entries with the correct server type */
@@ -264,11 +264,11 @@ void solicit(int force)
       ERROR(7,("  skip metaserver entries with INL type during non-INL mode\n"));
       continue;
     }
-    
+
     /* if we told metaserver recently, don't speak yet */
     if (!force)
       if ((now-m->sent) < m->minimum) continue;
-    
+
     /* don't remake the packet unless necessary */
     /* for INL the always recreate because we have multiple ports */
     if (inl_mode)
@@ -311,7 +311,7 @@ void solicit(int force)
       {
 	nfree = -queues[queue].count;
         gamefull++;
-      }      
+      }
       ERROR(7,("  nfree=%d nplayers=%d gamefull=%d\n", nfree, nplayers, gamefull));
 
 #ifdef RSA
@@ -350,7 +350,7 @@ void solicit(int force)
 	      );
       }
       here += strlen(here);
-      
+
       /* now append per-player information to the packet */
       for (j=0; j<MAXPLAYER; j++) {
 	/* ignore free slots and local players */
@@ -369,13 +369,13 @@ void solicit(int force)
         /* make sure name_fix() doesn't return NULL */
         name  = ( fixed_name != NULL )  ? fixed_name : players[j].p_name;
         login = ( fixed_login != NULL ) ? fixed_login : players[j].p_login;
-        
+
         /* if string is empty, report "unknown" */
         name  = ( *(name) == 0 )  ? unknown : name;
         login = ( *(login) == 0 ) ? unknown : login;
 
 	sprintf(here, "%c\n%c\n%d\n%d\n%s\n%s@%s\n",
-                /* number */   players[j].p_mapchars[1], 
+                /* number */   players[j].p_mapchars[1],
                 /* team   */   players[j].p_mapchars[0],
                 /* class  */   players[j].p_ship.s_type,
 		/* ??? note change from design, ship type number not string */
@@ -391,7 +391,7 @@ void solicit(int force)
     }
 
     ERROR(7,("  packet created for sending:\n%s\n", packet));
-    
+
     /* if we have exceeded the maximum time, force an update */
     if ((now-m->sent) > m->maximum) force=1;
 
@@ -402,7 +402,7 @@ void solicit(int force)
         ERROR(7,("  No change in packet since last time. dont send.\n"));
         continue;
       }
-    
+
     /* send the packet */
     if (udp_tx(m, packet, here-packet)) {
       m->sent=time(NULL);
