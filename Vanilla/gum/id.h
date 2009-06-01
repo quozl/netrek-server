7b8b5a5dea27b66078b761854b46b8e667f77b99
quozl <quozl@us.netrek.org>
2009-06-01 05:42:00 +0000

    remove gum
    gum is the deprecated administration GUI for the server, which has not
    been maintained for quite some time.

--- Vanilla/gum/id.h
+++
@@ -1,8 +0,0 @@
-void print_user __P ((uid_t uid));
-char* uid_to_string(uid_t uid);
-void print_group __P ((gid_t gid));
-char* gid_to_string(uid_t gid);
-int getugroups (int, gid_t *, char *);
-int get_all_groups(char*, gid_t **);
-int search_4group(gid_t, int, gid_t *);
-void *xmalloc (size_t n);
